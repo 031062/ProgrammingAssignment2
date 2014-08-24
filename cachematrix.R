a39ede4ec52a4c235092eebed939d2f086a92487
AH <aheimbs@yahoo.com>
2014-08-24 20:10:14 +0200

    Completed functions according to assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an example of lexical scoping, and how it can be used when the function's
+## environment is the body of another function.
 
-## Write a short comment describing this function
+## In the makeCacheMatrix function, the 4 functions set, get, setinverse and getinverse
+## are defined, as well as a list containing the 4 return values.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## The cacheSolve function checks whether the inverse of the matrix x 
+## has already been calculated.
+## If so, the cached inverse is returned, otherwise the inverse is
+## calculated via the solve function and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
